1518bef730b4309fa5be0b79f62a247ac38139ae
Joseph Radford <joseph@dosec.com.au>
2017-09-23 22:06:13 +1000

    List view model data and row count getter, setter of stack object

--- src/aptic_stack_parse/imagestacklistmodel.cpp
+++ src/aptic_stack_parse/imagestacklistmodel.cpp
@@ -1,6 +1,50 @@
 #include "imagestacklistmodel.h"
 
-ImageStackListModel::ImageStackListModel()
-{
+#include "imagestack.h"
+#include "imageobject.h"
 
+ImageStackListModel::ImageStackListModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    m_stack = 0;
+}
+
+QVariant ImageStackListModel::data(const QModelIndex &index, int role) const
+{
+    if (!m_stack) {
+        // this not valid, return empty variant
+        return QVariant();
+    }
+
+    if (index.row() >= this->rowCount(index)) {
+        // row is too big.
+        return QVariant();
+    }
+
+    if (role != Qt::DisplayRole) {
+        // not returning anything other than the raw data for now
+        return QVariant();
+    }
+
+    return QVariant(m_stack->imageAt(index.row())->filename());
+}
+
+int ImageStackListModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+
+    if (!m_stack) {
+        // object not set, so no rows
+        return 0;
+    }
+
+    // else
+    return m_stack->count();
+}
+
+void ImageStackListModel::updateStack(ImageStack *stack)
+{
+    emit layoutAboutToBeChanged();
+    m_stack = stack;
+    emit layoutChanged();
 }
--- src/aptic_stack_parse/imagestacklistmodel.h
+++ src/aptic_stack_parse/imagestacklistmodel.h
@@ -1,11 +1,21 @@
 #ifndef IMAGESTACKLISTMODEL_H
 #define IMAGESTACKLISTMODEL_H
 
+#include <QAbstractListModel>
 
-class ImageStackListModel
+class ImageStack;
+
+class ImageStackListModel : public QAbstractListModel
 {
 public:
-    ImageStackListModel();
+    ImageStackListModel(QObject *parent = 0);
+
+    QVariant data(const QModelIndex &index, int role) const;
+    int rowCount(const QModelIndex &parent) const;
+
+    void updateStack(ImageStack *stack);
+private:
+    ImageStack *m_stack;
 };
 
 #endif // IMAGESTACKLISTMODEL_H
