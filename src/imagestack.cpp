d9c56426b663790c21c0af0d8250a403f48c2e96
Joseph Radford <joseph@dosec.com.au>
2017-09-24 11:19:18 +1000

    Copy image data into a separate greyscale vector object

--- src/imagestack.cpp
+++ src/imagestack.cpp
@@ -3,9 +3,76 @@
 #include <QtDebug>
 #include "imageobject.h"
 
+ImageStackRaw::ImageStackRaw()
+{
+    m_samples = 0;
+    m_rows = 0;
+    m_columns = 0;
+}
+
+void ImageStackRaw::setStackArray(const QList<ImageObject *> &images)
+{
+    // go through all the images in the stack and create a 3D greyscale array
+
+    // clear all the parsed data
+
+    if (images.count() == 0) {
+        // nothing to process
+        return;
+    }
+
+    // the accepted row and column count is decided by the first image
+    // if any subsequent images do not match, they will be ignored
+    m_rows = images.first()->pixmap().height();
+    m_columns = images.first()->pixmap().width();
+
+
+    for (int i = 0; i < images.count(); i++) {
+        QImage img = images.at(i)->pixmap().toImage();
+        if ((img.height() == m_rows) && (img.width() == m_columns)) {
+
+            if (img.isGrayscale() == false) {
+                // convert
+                convertToGreyscale(&img);
+
+            }
+            else {
+                // copy straight in
+            }
+
+            unsigned char *imgBytes = img.bits();
+            // only need every 4th. RGBA, only need one.
+            for (int j = 0; j < img.byteCount(); j += 4) {
+                m_data.push_back(imgBytes[j]);
+            }
+
+            // more checking may be needed here if different types of images start getting imported
+            // crucially, the output must be greyscale, with one byte representing the intensity of one pixel
+
+            m_samples++;
+        }
+    }
+
+    qDebug () << m_data.size();
+    qDebug() << m_samples;
+}
+
+// from https://stackoverflow.com/questions/27949569/convert-a-qimage-to-grayscale
+void ImageStackRaw::convertToGreyscale(QImage *image) {
+    for (int ii = 0; ii < image->height(); ii++) {
+        uchar* scan = image->scanLine(ii);
+        int depth =4;
+        for (int jj = 0; jj < image->width(); jj++) {
+
+            QRgb* rgbpixel = reinterpret_cast<QRgb*>(scan + jj*depth);
+            int gray = qGray(*rgbpixel);
+            *rgbpixel = QColor(gray, gray, gray).rgba();
+        }
+    }
+}
+
 ImageStack::ImageStack()
 {
-
 }
 
 void ImageStack::setFileNames(const QStringList &filenames)
@@ -22,5 +89,6 @@ void ImageStack::setFileNames(const QStringList &filenames)
     qDebug() << "added to list: " << m_images.count();
 
     // convert stack into a 3D greyscale array
-    //m_images.at(0)->pixmap().toImage().
+    m_stackArray.setStackArray(m_images);
 }
+
--- src/imagestack.h
+++ src/imagestack.h
@@ -3,7 +3,30 @@
 
 #include <QList>
 
+class QImage;
+
 class ImageObject; ///< each instance is image name and data
+
+class ImageStackRaw
+{
+public:
+    ImageStackRaw();
+
+    unsigned char data(int row, int column, int sample) const;
+
+    int samples() const { return m_samples; }
+    int rows()    const { return m_rows; }
+    int columns() const { return m_columns; }
+
+    void setStackArray(const QList<ImageObject*> &images);
+private:
+    std::vector<unsigned char> m_data;
+    int m_rows;
+    int m_columns;
+    int m_samples;
+
+    void convertToGreyscale(QImage *image);
+};
 
 /*!
  * \brief The ImageStack class manages a list of \sa ImageObject
@@ -21,6 +44,8 @@ public:
 
 private:
     QList<ImageObject*> m_images;
+
+    ImageStackRaw m_stackArray;
 };
 
 #endif // IMAGESTACK_H
