95d7690ae2d6a89e6a743ee68cf07b57649e319b
Joseph Radford <joeradford@gmail.com>
2018-10-31 20:56:07 +1100

    Get minimum to work with boost
    ImageObject compiles
    Requires warning suppression to compile parts of boost

--- src/ApticImage/imageobject.cpp
+++ src/ApticImage/imageobject.cpp
@@ -1,11 +1,14 @@
 #include "imageobject.h"
 
-#include <QFileInfo>
+using namespace boost::gil;
 
-ImageObject::ImageObject(const QString &filename)
+ImageObject::ImageObject(const std::string &filename)
 {
     m_filename = filename;
-    QFileInfo fi(m_filename);
-    m_displayName = fi.baseName();
-    m_pixmap = QPixmap(m_filename);
+	rgb8_image_t img;
+	read_image(filename, img, bmp_tag());
+
+    //QFileInfo fi(m_filename);
+    //m_displayName = fi.baseName();
+    //m_pixmap = QPixmap(m_filename);
 }
--- src/ApticImage/imageobject.h
+++ src/ApticImage/imageobject.h
@@ -1,8 +1,10 @@
 #ifndef IMAGEOBJECT_H
 #define IMAGEOBJECT_H
 
-#include <QString>
-#include <QPixmap>
+#include <string>
+#include <boost/gil.hpp>
+#include <boost/gil/gil_all.hpp>
+#include <boost/gil/extension/io/bmp.hpp>
 
 /*!
  * \brief The ImageObject class contains metadata about an image in the stack as well as the image data itself
@@ -10,16 +12,16 @@
 class ImageObject
 {
 public:
-    ImageObject(const QString &filename);
+    ImageObject(const std::string &filename);
 
-    QString filename()    const { return m_filename; }
-    QString displayName() const { return m_displayName; }
-    QPixmap pixmap()      const { return m_pixmap; }
+    std::string  filename()    const { return m_filename; }
+	std::string  displayName() const { return m_displayName; }
+	boost::gil::rgb8_image_t image()       const { return m_image; }
 
 private:
-    QString m_filename; ///< The full filename for this image
-    QString m_displayName;
-    QPixmap m_pixmap;   ///< image data
+	std::string m_filename; ///< The full filename for this image
+	std::string m_displayName;
+	boost::gil::rgb8_image_t m_image;
 };
 
 #endif // IMAGEOBJECT_H
